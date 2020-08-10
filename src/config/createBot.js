c20f5fc4e4e4b7fa30f24d723d6d0445bf551f81
Abraao Rodrigues <abraao503@gmail.com>
2020-08-10 08:05:26 -0400

    update create bot function

--- src/config/createBot.js
+++ src/config/createBot.js
@@ -4,7 +4,7 @@ function CreateBot(){
   const bot = new Telegraf(process.env.BOT_TOKEN);
 
   if(process.env.NODE_ENV === 'production'){
-    bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
+    bot.telegram.setWebhook(`${process.env.URL}/bot${process.env.BOT_TOKEN}`);
     bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
   }
 
