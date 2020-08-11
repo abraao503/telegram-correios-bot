20819eb214cbab493e2940e8545413048f858dd1
Abraao Rodrigues <abraao503@gmail.com>
2020-08-11 18:48:40 -0400

    adjuste for deploy

--- src/config/createBot.js
+++ src/config/createBot.js
@@ -4,9 +4,9 @@ function CreateBot(){
   const bot = new Telegraf(process.env.BOT_TOKEN);
 
   if(process.env.NODE_ENV === 'production'){
-    bot.telegram.setWebhook(`${process.env.URL}/bot${process.env.BOT_TOKEN}`);
+    bot.telegram.setWebhook(`${process.env.DEPLOY_URL}/bot${process.env.BOT_TOKEN}`);
     bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
-    console.log(process.env.URL);
+    console.log(process.env.DEPLOY_URL);
   }
 
   return bot;
